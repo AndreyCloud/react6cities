fcbdc77827ae12e9367d4795034c2334e8195fc2
AndreyCloud <d.cloud@mail.ru>
2022-05-30 22:54:08 +0300

    Map

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -4,42 +4,101 @@ import { ArrPlaces } from '../types/types';
 export const places: ArrPlaces = [
   {
     id: 1,
-    name:'Beautiful &amp; luxurious apartment at great location',
+    name: 'Beautiful &amp; luxurious apartment at great location',
     mark: 'Premium',
     img: 'img/apartment-01.jpg',
     price: 120,
     priceText: 'night',
     type: 'Apartment',
     favorite: true,
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.85309666406198,
+      zoom: 8,
+      city: {
+        location: {
+          latitude: 52.370216,
+          longitude: 4.895168,
+          zoom: 10,
+        },
+        name: 'Amsterdam',
+      },
+    },
   },
   {
     id: 2,
-    name:'Wood and stone place',
+    name: 'Wood and stone place',
     mark: '',
     img: 'img/room.jpg',
     price: 80,
     priceText: 'night',
     type: 'Private room',
     favorite: false,
+    location: {
+      latitude: 52.369553943508,
+      longitude: 4.85309666406198,
+      zoom: 8,
+      city: {
+        location: {
+          latitude: 52.370216,
+          longitude: 4.895168,
+          zoom: 10,
+        },
+        name: 'Amsterdam',
+      },
+    },
   },
   {
     id: 3,
-    name:'Canal View Prinsengracht',
+    name: 'Canal View Prinsengracht',
     mark: 'Premium',
     img: 'img/apartment-02.jpg',
     price: 132,
     priceText: 'night',
     type: 'Apartment',
     favorite: true,
+    location: {
+      latitude: 52.3909553943508,
+      longitude: 4.929309666406198,
+      zoom: 8,
+      city: {
+        location: {
+          latitude: 52.370216,
+          longitude: 4.895168,
+          zoom: 10,
+        },
+        name: 'Amsterdam',
+      },
+    },
   },
   {
     id: 4,
-    name:'Nice, cozy, warm big bed apartment',
+    name: 'Nice, cozy, warm big bed apartment',
     mark: 'Premium',
     img: 'img/apartment-03.jpg',
     price: 180,
     priceText: 'night',
     type: 'Apartment',
     favorite: false,
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 8,
+      city: {
+        location: {
+          latitude: 52.370216,
+          longitude: 4.895168,
+          zoom: 10,
+        },
+        name: 'Amsterdam',
+      },
+    },
   },
 ];
+
+// export const CITY: City = {
+//   title: 'Amsterdam',
+//   lat: 52.374,
+//   lng: 4.88969,
+//   zoom: 8,
+// };
--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,3 +1,9 @@
+type locationCity = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+};
+
 export type ArrPlaces = {
   id: number;
   name: string;
@@ -7,4 +13,23 @@ export type ArrPlaces = {
   priceText: string;
   type: string;
   favorite: boolean;
-}[]
+  location: {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+    city: City;
+  };
+}[];
+
+export type City = {
+  location: locationCity;
+  name: string;
+};
+
+// export type Point = {
+//   title: string;
+//   lat: number;
+//   lng: number;
+// };
+
+// export type Points = Point[];
