f996ec95efd1fb311a3445f3eaa103bd027e3b6d
AndreyCloud <d.cloud@mail.ru>
2022-06-30 23:01:14 +0300

    Valid

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks/useApps';
 import { fetchLogin } from '../../store/userSlice';
@@ -7,6 +7,62 @@ function Login(): JSX.Element {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState('Email cannot be empty!');
+  const [passwordlError, setPasswordError] = useState('Password cannot be empty!');
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (emailError || passwordlError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, passwordlError]);
+
+  const blurHandler = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+    switch(e.target.name) {
+      case 'email':
+        setEmailDirty(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        break;
+    }
+  };
+
+  const EmailHandler = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+
+    setEmail(e.target.value);
+
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError('not correct email');
+      if(!e.target.value) {
+        setEmailError('Email cannot be empty!');
+      }
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const PasswordHandler = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+
+    setPassword(e.target.value);
+
+    const re = /(?=.*[0-9])(?=.*[a-z])/g;
+
+    if(!re.test(String(e.target.value).toLowerCase())) {
+      setPasswordError('must have at least one letter and one number');
+      if(!e.target.value) {
+        setPasswordError('Password cannot be empty!');
+      }
+    } else {
+      setPasswordError('');
+    }
+  };
 
 
   const user = {email, password};
@@ -43,30 +99,33 @@ function Login(): JSX.Element {
             <h1 className="login__title">Sign in</h1>
             <form onSubmit={sendLogin} className="login__form form" action="#" method="post">
               <div className="login__input-wrapper form__input-wrapper">
+                {(emailDirty && emailError) && <p style={{color: 'red'}}>{emailError}</p>}
                 <label className="visually-hidden">E-mail</label>
                 <input
+                  onBlur={(e) => blurHandler(e)}
                   className="login__input form__input"
                   type="email"
                   name="email"
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => EmailHandler(e)}
                   placeholder="Email"
-                  required
                 />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
+                {(passwordDirty && passwordlError) && <p style={{color: 'red'}}>{passwordlError}</p>}
                 <label className="visually-hidden">Password</label>
                 <input
+                  onBlur={(e) => blurHandler(e)}
                   className="login__input form__input"
                   type="password"
                   name="password"
                   value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => PasswordHandler(e)}
                   placeholder="Password"
-                  required
                 />
               </div>
               <button
+                disabled = {!formValid}
                 className="login__submit form__submit button"
               >
                   Sign in
